f8f4506c2d73a2e1a1c3e8537dab30372177dc95
dhananjoycd <dcdmf2020@gmail.com>
2022-05-24 19:52:03 +0600

    login and signUp done

--- src/Components/Header/Login/Login.js
+++ src/Components/Header/Login/Login.js
@@ -1,12 +1,109 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Nav } from 'react-bootstrap';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../../OtherPages/PageNotFound/Loading';
 import SocialLogin from '../SocialLogin';
 
 const Login = () => {
-    return (
-        <div>
-   <SocialLogin></SocialLogin>
-        </div>
-    );
+  //input data value receive from user
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+   const [sendPasswordResetEmail, sendingRest, errorRest] = useSendPasswordResetEmail(
+       auth
+     );
+   const navigate = useNavigate();
+   const location = useLocation();
+   let from = location.state?.from?.pathname || "/";
+  //email and password firebase hooks -- new user create
+    const [
+           signInWithEmailAndPassword,
+           user,
+           loading,
+           error,
+         ] = useSignInWithEmailAndPassword(auth);
+     
+     
+      
+   
+       let errorAce;
+         if(error || errorRest ){
+             errorAce = error?.message || errorRest?.message;
+         }
+   
+       if(loading || sendingRest){
+  return <Loading></Loading>
+       }
+   
+   
+       if(user){
+             navigate(from, { replace: true });
+       }
+   
+       const handleFormSubmit = async e =>{
+           e.preventDefault();  
+           const email = emailRef.current.value;
+           const password = passwordRef.current.value;
+
+           if(!error || !errorRest ){
+             signInWithEmailAndPassword(email, password);
+       
+           }
+           
+    else{
+      alert(`You have some error like ${errorAce}`)
+    }
+         }
+   
+       return (
+           <div className='container'>
+   
+            <p className='font-body text-danger text-center pt-2'>   {
+                   errorAce
+               }</p>
+   
+       <div className='bg-light font-title text-center py-3 rounded'>
+         <span className='bg-danger text-white fw-bold rounded-pill px-2 py-1'>Log In Here</span> <hr />
+       <form onSubmit={handleFormSubmit}>
+     
+   
+     <div className='mb-2'>
+         <label htmlFor="email">Email</label> <br />
+         <input ref={emailRef} required type="email" name="email" id="" placeholder='Email'/>
+     </div>  
+          
+       <div className='mb-2'>
+         <label htmlFor="password">Password</label> <br />
+         <input ref={passwordRef} required type="password" name="password" id="" placeholder='Password'/>
+     </div> 
+ 
+ 
+     <input className='mb-2 btn btn-primary' type="submit" value="Login" />  
+          
+     </form>  
+
+     <div>
+       <span className='d-flex align-items-center justify-content-center'>Have You need a New Account? <Nav.Link className='text-danger bg-warning rounded-pill ms-1' as={Link} to="/signup">Sign Up</Nav.Link></span>
+     <h5 className='fs-6 my-2'> Do you forget Your password?   <button className='ms-3 p-2 btn btn-danger'
+     onClick={async () => {
+         const email = emailRef.current.value;
+       await sendPasswordResetEmail(email);
+       alert('Sent email for Reset Password');
+     }}
+   >
+     Forget password
+   </button></h5>
+     
+     </div>     
+     <hr />
+<div>
+    <h6>Login By using Socail Account</h6>
+    <SocialLogin></SocialLogin>
+</div>
+       </div>
+           </div>
+       );
 };
 
 export default Login;
--- src/Components/Header/SignUp/SignUp.js
+++ src/Components/Header/SignUp/SignUp.js
@@ -1,11 +1,133 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Nav } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../../OtherPages/PageNotFound/Loading';
+import SocialLogin from '../SocialLogin';
 
 const SignUp = () => {
+  //input data value receive from user
+  const nameRef = useRef('');
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const confirmPassworRef = useRef('');
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+    //email and password firebase hooks -- new user create
+ 
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+     
+  
+    
+    let errorAce;
+      if(error || updateError ){
+          errorAce = error?.message || updateError?.message;
+      }
+      // console.log(errorAce)
+
+    if(loading || updating){
+        return <Loading></Loading>
+    }
+
+    if(user){
+     //navigate korte hobe
+     navigate(from, { replace: true });
+    }
+
+
+    const handleFormSubmit = async e =>{
+        e.preventDefault();  
+        const displayName = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const surePass = confirmPassworRef.current.value;
+        
+        if(password === surePass) {
+
+          if(!error){
+           if(!updateError){
+            await  createUserWithEmailAndPassword(email, password);
+            await updateProfile({ displayName});
+            alert('Your Profile Created');
+            window.location.reload();
+           }
+           else{
+            
+            alert(`${errorAce}`);
+            window.location.reload();
+           }
+         
+          }
+         else{
+          alert(`You have already signup, Please Login`);
+          window.location.reload();
+
+         }
+       
+        }
+        else{
+          alert(`Password does not match`);
+          window.location.reload();
+
+        }
+      }
+
     return (
-        <div>
-            SignUp
+        <div className='container'>
+
+<p className='font-body text-danger text-center pt-2'>   {
+                    errorAce
+                }</p>
+
+   <div className='bg-light text-center py-3 font-title rounded'>
+   <span className='text-white bg-danger fw-bold rounded-pill px-2 py-1'>Sign Up Here</span> <hr />
+   <form onSubmit={handleFormSubmit}>
+  
+
+  <div className='mb-2'>
+      <label htmlFor="displayName">Your Full Name</label> <br />
+      <input ref={nameRef} required type="text" name="displayName" id="" placeholder='Your Name' autoComplete='off'/>
+  </div>  
+
+  <div className='mb-2'>
+      <label htmlFor="email">Email</label> <br />
+      <input ref={emailRef} required type="email" name="email" id="" placeholder='Email' autoComplete='off'/>
+  </div>  
+       
+    <div className='mb-2'>
+      <label htmlFor="password">Password</label> <br />
+      <input ref={passwordRef} required type="password" name="password" id="" placeholder='Password' autoComplete='off'/>
+  </div> 
+
+    <div className='mb-2'>
+      <label htmlFor="confirm-password">Confirm Your Password</label> <br />
+      <input ref={confirmPassworRef} required type="password" name="confirm-password" id="" placeholder='Password' autoComplete='off'/>
+  </div>  
+
+  <input className='mb-2 btn btn-primary' type="submit" value="Sign Up" />  
+       
+  </form>       
+ 
+ {/* in the below the component for social LogIn */}
+ <span className='d-flex align-items-center justify-content-center'>Have You already an Account? <Nav.Link className='text-danger bg-warning fw-bold rounded-pill ms-1'  as={Link} to="/login">Log In</Nav.Link></span>
+ <hr />
+<div>
+    <h6>Login By using Socail Account</h6>
+    <SocialLogin></SocialLogin>
+</div>
+   </div>
         </div>
     );
 };
+
 
 export default SignUp;
