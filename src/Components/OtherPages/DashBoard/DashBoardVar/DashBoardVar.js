04282fb334d34d6ac2bcde3f25c26519bbdfdd59
dhananjoycd <dcdmf2020@gmail.com>
2022-05-26 13:27:45 +0600

    Make Admin Done

--- src/Components/OtherPages/DashBoard/DashBoardVar/DashBoardVar.js
+++ src/Components/OtherPages/DashBoard/DashBoardVar/DashBoardVar.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../../../firebase.init';
+import useGetPost from '../../../../Hooks/useGetPost';
 
 const DashBoardVar = () => {
+  const [user, loading, error] = useAuthState(auth);
+  //get correct user
+let userUrl = 'http://localhost:5000/users'
+const {posts} = useGetPost(userUrl);
+ const dbUser = posts.find(p => {
+    if(p?.uid === user?.uid  ){
+        // dbUser.unshift(p)
+        return p;
+    }
+})
+
     return (
         <>
   {['sm'].map((expand) => (
@@ -22,13 +36,18 @@ const DashBoardVar = () => {
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link as={Link} to="/dashboard/myorders">My Orders</Nav.Link>
-              <Nav.Link as={Link} to="/dashboard/addreview">Add Review</Nav.Link>
+            
               <Nav.Link as={Link} to="/dashboard/myprofile">My Profile</Nav.Link>
-              <Nav.Link as={Link} to="/dashboard/manageOrders">Manage Orders</Nav.Link>
+          {
+            (dbUser?.role === 'Admin')? <>
+                <Nav.Link as={Link} to="/dashboard/manageOrders">Manage Orders</Nav.Link>
               <Nav.Link as={Link} to="/dashboard/addProducts">Add Products</Nav.Link>
               <Nav.Link as={Link} to="/dashboard/manageProducts">Manage Products</Nav.Link>
-              <Nav.Link as={Link} to="/dashboard/admin">Make Admin</Nav.Link>
+              <Nav.Link as={Link} to="/dashboard/admin">Make Admin</Nav.Link></>: <>
+              <Nav.Link as={Link} to="/dashboard/myorders">My Orders</Nav.Link>
+              <Nav.Link as={Link} to="/dashboard/addreview">Add Review</Nav.Link>
+              </>
+          }
            
             </Nav>
            
--- src/Components/OtherPages/DashBoard/MakeAdmin/MakeAdmin.js
+++ src/Components/OtherPages/DashBoard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+import auth from '../../../../firebase.init';
+import useGetPost from '../../../../Hooks/useGetPost';
+import useUpdatePost from '../../../../Hooks/useUpdatePost';
+
 
 const MakeAdmin = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const {updateApi, updateDone} = useUpdatePost();
+    //get correct user
+  let userUrl = 'http://localhost:5000/users'
+  const {posts} = useGetPost(userUrl);
+   let dbUsers = posts;
+
     return (
         <div>
-            MakeAdmin
+    <div className="table-responsive-md">
+        <h6 className='text-center'>We Have <span className='text-danger'>{dbUsers?.length}</span>  users with Admin </h6>
+    <table className="table table-striped">
+    <thead>
+    <tr>
+      <th scope="col">SN.</th>
+      <th scope="col">User Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Status</th>
+      <th className='bg-warning rounded' scope="col"  colspan="2">Admin Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  {
+      dbUsers.map((dbUser, index) => <tr>
+        <th scope="row">{index+1}</th>
+        <td>{dbUser?.displayName}</td>
+        <td>{dbUser?.email}</td>
+        <td>{dbUser?.role ||'Viewer'}</td>
+
+        <td> {
+            (dbUser?.role ==='Admin') ? <Button variant="success" className='fw-bold b-title' size="sm" disabled>
+          Already Admin
+        </Button>:
+        <Button onClick={()=>{
+            const data ={ role: "Admin"}
+             updateApi(`http://localhost:5000/users/${dbUser._id}`, data, user?.uid, 'Admin' )
+             {
+              updateDone&&  toast.success('Your Profile was Updated Successfully');
+             }
+        }} variant="success" className='fw-bold b-title' size="sm">
+      Make Admin
+    </Button>
+            }</td>
+
+        <td> <Button variant="danger" className='fw-bold b-title' size="sm">
+    Remove User
+    </Button></td>
+      </tr>)
+  }
+
+  </tbody>
+
+    </table>
+    </div>
         </div>
     );
 };
